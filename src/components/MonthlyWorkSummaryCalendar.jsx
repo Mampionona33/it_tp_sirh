5379e820bb9e9c140d9945aa760dcf5c4e1bec0e
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-10-25 09:06:43 +0000

    Create MonthlyWorkSummaryCalendar.jsx

---
+++ src/components/MonthlyWorkSummaryCalendar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const MonthlyWorkSummaryCalendar = () => {
+  return (
+    <>
+      <div className="flex">
+        <p>calendar</p>
+      </div>
+    </>
+  )
+}
+
+export default MonthlyWorkSummaryCalendar
