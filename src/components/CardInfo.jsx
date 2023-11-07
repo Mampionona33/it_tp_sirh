925e755443aaf794d5169b82be3035b3124606a6
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-07 10:43:38 +0000

    Update 3 files

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types'
 const CardInfo = (props) => {
   return (
     <>
-      <div>
-        <h4>{props.title}</h4>
+      <div className="flex align-bottom justify-between px-2 py-3">
+        <h4 className="text-lg font-medium p-0 m-0">{props.title}</h4>
+        <p className="p-0 m-0">{props.body}</p>
       </div>
     </>
   )
--- src/components/SalaryCalculation/GrossWages.js
+++ src/components/SalaryCalculation/GrossWages.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import CustomSection from 'src/components/CustomSection'
 import { useSelector } from 'react-redux'
+import CardInfo from 'src/components/CardInfo'
 
 const GrossWages = () => {
   const employeeTotalHours = useSelector((state) => state.employeHours)
   const title = 'Salaire brute'
   const body = 'body'
 
+  const Body = () => {
+    return (
+      <>
+        <CardInfo title={'info 1'} body={'body info'} />
+      </>
+    )
+  }
+
   return (
     <>
       <div>
-        <CustomSection title={title} body={body} />
+        <CustomSection title={title} body={<Body />} />
       </div>
     </>
   )
