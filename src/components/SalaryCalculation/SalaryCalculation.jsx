700b7060eb9d12c639621e111baa586240d495b0
Mampionona33 <ramamps33@gmail.com>
2023-11-08 21:59:20 +0300

    créer prime et avantage

--- src/components/SalaryCalculation/SalaryCalculation.jsx
+++ src/components/SalaryCalculation/SalaryCalculation.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import GrossWages from 'src/components/SalaryCalculation/GrossWages'
 import SalaireNet from 'src/components/SalaryCalculation/SalaireNet'
-import PropTypes from 'prop-types'
+import PrimeEtAvantage from './PrimeEtAvantage'
 
 const SalaryCalculation = () => {
   return (
     <>
-      <div className="flex justify-evenly columns-2">
-        <GrossWages />
-        <SalaireNet />
+      <div className="flex flex-col">
+        <div className="flex flex-wrap justify-evenly columns-2">
+          <GrossWages />
+          <SalaireNet />
+        </div>
+        <div className="flex">
+          <PrimeEtAvantage />
+        </div>
       </div>
     </>
   )
