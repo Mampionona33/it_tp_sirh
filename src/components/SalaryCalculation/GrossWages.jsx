1108e2c0ee5b9611b40e4074416ac8703d4219b5
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-08 06:13:30 +0000

    Update tasks.json and GrossWages.jsx

--- src/components/SalaryCalculation/GrossWages.jsx
+++ src/components/SalaryCalculation/GrossWages.jsx
@@ -7,43 +7,76 @@ const GrossWages = () => {
   const title = 'Salaire brute'
   const selecteEmploy = useSelector((state) => state.selectedEmploye.employe)
 
-  console.log(employeeTotalHours)
+  const tauxHoraire = selecteEmploy.salaireBase / 173.33
+
+  const calculHs = (heures, majoration) => {
+    let valeur = 0
+    valeur = (tauxHoraire * heures * majoration) / 100
+    return valeur
+  }
+  const hsni130Value = calculHs(employeeTotalHours.hsni130, 130)
+  const hsni150Value = calculHs(employeeTotalHours.hsni150, 150)
+  const hsi130Value = calculHs(employeeTotalHours.totalHs130 - employeeTotalHours.hsni130, 130)
+  const hsi150Value = calculHs(employeeTotalHours.totalHs150 - employeeTotalHours.hsni150, 150)
+  const hn30Value = calculHs(employeeTotalHours.totalHs30, 30)
+  const hn50Value = calculHs(employeeTotalHours.totalHs50, 50)
+  const hdimValue = calculHs(employeeTotalHours.totalHdim, 100)
+
+  const salaireBruteVal =
+    hsni130Value + hsni150Value + hsi130Value + hsi150Value + hn30Value + hn50Value + hdimValue
+
+  const formatToMoney = (number) => {
+    return new Intl.NumberFormat('fr', {
+      style: 'currency',
+      currency: 'MGA',
+      currencyDisplay: 'narrowSymbol',
+    }).format(number)
+  }
+
+  const formatedHsni130Value = formatToMoney(hsni130Value)
+  const formatedHsni150Value = formatToMoney(hsni150Value)
+  const formatedHsi130Value = formatToMoney(hsi130Value)
+  const formatedHsi150Value = formatToMoney(hsi150Value)
+  const formatedHn30Value = formatToMoney(hn30Value)
+  const formatedHn50Value = formatToMoney(hn50Value)
+  const formatedHdimValue = formatToMoney(hdimValue)
+  const formatedSlaireBruteValue = formatToMoney(salaireBruteVal)
 
   const data = [
     {
       title: 'HSNI 130% :',
       hours: `${employeeTotalHours.hsni130}`,
-      value: `100000 Ar`,
+      value: `${formatedHsni130Value}`,
     },
     {
       title: 'HSNI 150% :',
       hours: `${employeeTotalHours.hsni150}`,
-      value: `100000 Ar`,
+      value: `${formatedHsni150Value}`,
     },
     {
       title: 'HSI 130% :',
       hours: `${employeeTotalHours.totalHs130 - employeeTotalHours.hsni130}`,
-      value: `100000 Ar`,
+      value: `${formatedHsi130Value}`,
     },
     {
       title: 'HSI 150% :',
       hours: `${employeeTotalHours.totalHs150 - employeeTotalHours.hsni150}`,
-      value: `100000 Ar`,
+      value: `${formatedHsi150Value}`,
     },
     {
       title: 'HN 30% :',
       hours: `${employeeTotalHours.totalHs30}`,
-      value: `3000000 Ar`,
+      value: `${formatedHn30Value}`,
     },
     {
       title: 'HN 50% :',
       hours: `${employeeTotalHours.totalHs50}`,
-      value: `1500 Ar`,
+      value: `${formatedHn50Value}`,
     },
     {
       title: 'Hdim% :',
       hours: `${employeeTotalHours.totalHdim}`,
-      value: `10000 Ar`,
+      value: `${formatedHdimValue}`,
     },
   ]
 
@@ -70,7 +103,9 @@ const GrossWages = () => {
             <td colSpan="2" className="text-left py-3 pl-4 font-medium">
               Salaire brute
             </td>
-            <td className="text-right py-3 pr-4 font-medium text-customRed-900">10000 Ar</td>
+            <td className="text-right py-3 pr-4 font-medium text-customRed-900">
+              {formatedSlaireBruteValue}
+            </td>
           </tr>
         </tbody>
       </table>
