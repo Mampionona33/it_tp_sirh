e8df9c33640aa1081b7b4942f0f80a0c9bca28db
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-09 05:36:20 +0000

    Update SalaireNetAPayer.jsx

--- src/components/SalaryCalculation/SalaireNetAPayer.jsx
+++ src/components/SalaryCalculation/SalaireNetAPayer.jsx
@@ -2,15 +2,34 @@ import React from 'react'
 import CustomSection from 'src/components/CustomSection'
 import { useSelector } from 'react-redux'
 import formatAriaryMga from 'src/utils/formatAriaryMga'
+import { cotisastions } from 'src/db/db'
 
 const SalaireNetAPayer = () => {
   const title = 'Salaire net à payer'
   const salaireBrut = useSelector((state) => state.selectedEmploye.salaireBrut)
+  const cnaps = cotisastions.filter((item) => item.id === 1)
+  const ostie = cotisastions.filter((item) => item.id === 2)
 
   const data = [
     {
       title: 'Salaire brut :',
-      value: <span className="font-medium">{formatAriaryMga(salaireBrut)}</span>,
+      value: `${formatAriaryMga(salaireBrut)}`,
+    },
+    {
+      title: 'CNAPS :',
+      value: `${formatAriaryMga(cnaps)}`,
+    },
+    {
+      title: 'OSTIE :',
+      value: `${formatAriaryMga(ostie)}`,
+    },
+    {
+      title: 'IRSA :',
+      value: `${formatAriaryMga(salaireBrut)}`,
+    },
+    {
+      title: 'Salaire net à payer :',
+      value: `${formatAriaryMga(salaireBrut)}`,
     },
   ]
 
