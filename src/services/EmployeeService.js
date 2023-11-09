4e18779ce9e02d31b2d39d73f16136dc5f3e7192
Mampionona33 <ramamps33@gmail.com>
2023-11-09 21:30:38 +0300

    change in fiche

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -6,6 +6,9 @@ class EmployeeService {
   getAll() {
     return axios.get(`${API_BASE_URL}/employees/list`)
   }
+  getById(id) {
+    return axios.get(`${API_BASE_URL}/employees/id=${id}`)
+  }
 }
 
 export default new EmployeeService()
