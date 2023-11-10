0feb41619606ebb4337303fcdd5f5d205935bfe8
Mampionona33 <ramamps33@gmail.com>
2023-11-10 14:32:49 +0000

    try to setup italic

--- src/views/employee/bulletinPaie/BulletinPaie.jsx
+++ src/views/employee/bulletinPaie/BulletinPaie.jsx
@@ -1,29 +1,69 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import ReactPDF, { Page, Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer'
+import ReactPDF, {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  PDFViewer,
+  Font,
+} from '@react-pdf/renderer'
+
+// Font.register({
+//   src: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap',
+//   fontStyle: 'italic',
+//   family: 'Roboto',
+// })
 
 // Create styles
 const styles = StyleSheet.create({
   header: {
     textAlign: 'center',
+    fontSize: 10,
+    fontWeight: 'demibold',
   },
   table: {
     display: 'table',
     width: 'auto',
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderRightWidth: 0,
-    borderBottomWidth: 0,
+    border: 'collapse',
   },
   tableRow: {
     flexDirection: 'row',
   },
-  tableCol: {
-    width: '50%',
+  cellBorderLeft: {
+    borderStyle: 'solid',
+    borderLeft: 1,
+  },
+  cellBorderRight: {
+    borderStyle: 'solid',
+    borderRight: 1,
+  },
+  cellBorderTop: {
+    borderStyle: 'solid',
+    borderTop: 1,
+  },
+  cellBorderBottom: {
+    borderStyle: 'solid',
+    borderBottom: 1,
+  },
+
+  bordered: {
     borderStyle: 'solid',
     borderWidth: 1,
+  },
+  tableCol: {
+    width: '50%',
     borderLeftWidth: 0,
     borderTopWidth: 0,
+  },
+  italicTableCell: {
+    textAlign: 'left',
+    padding: 2,
+    paddingTop: 1,
+    fontSize: 10,
+    // fontFamily: 'Roboto',
+    fontStyle: 'italic',
   },
   tableCell: {
     textAlign: 'left',
@@ -43,11 +83,27 @@ const MyDocument = () => {
         </View>
         <View style={styles.table}>
           <View style={styles.tableRow}>
-            <View style={styles.tableCol}>
+            <View style={[styles.tableCol, styles.cellBorderTop, styles.cellBorderBottom]}>
               <Text style={styles.tableCell}>EMPLOYEUR: </Text>
             </View>
-            <View style={styles.tableCol}>
+            <View style={[styles.tableCol, styles.bordered]}>
               <Text style={styles.tableCell}>SALARIE: </Text>
+            </View>
+          </View>
+          <View style={styles.tableRow}>
+            <View style={[styles.tableCol, styles.italicTableCell]}>
+              <Text style={[styles.italicTableCell]}>Nom:</Text>
+            </View>
+            <View style={[styles.tableCol, styles.cellBorderLeft]}>
+              <Text style={styles.tableCell}>Nom et Prénom:</Text>
+            </View>
+          </View>
+          <View style={styles.tableRow}>
+            <View style={styles.tableCol}>
+              <Text style={styles.tableCell}>Adresse:</Text>
+            </View>
+            <View style={[styles.tableCol, styles.cellBorderLeft]}>
+              <Text style={styles.tableCell}>Fonction:</Text>
             </View>
           </View>
         </View>
