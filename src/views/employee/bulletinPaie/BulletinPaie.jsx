9e11e2c794bf483b15b0cdd1ef576bcf223006cc
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-10 09:04:25 +0000

    Update BulletinPaie.jsx

--- src/views/employee/bulletinPaie/BulletinPaie.jsx
+++ src/views/employee/bulletinPaie/BulletinPaie.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import ReactPDF, { Page, Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer'
+
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: 'row',
+    backgroundColor: '#E4E4E4',
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+})
+
+// Create Document Component
+const MyDocument = () => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <Text>Section #1</Text>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+)
 
 const BulletinPaie = () => {
   const selecteEmploy = useSelector((state) => state.selectedEmploye.employe)
-
   return (
     <>
-      <div>BulletinPaie de {selecteEmploy.name.nom}</div>
+      <PDFViewer>
+        <MyDocument />
+      </PDFViewer>
     </>
   )
 }
