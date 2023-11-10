36dc2f18868e0b28ada3ec24833c9ddfc6995a24
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-10 08:05:32 +0000

    Update SalaryCalculation.jsx

--- src/components/SalaryCalculation/SalaryCalculation.jsx
+++ src/components/SalaryCalculation/SalaryCalculation.jsx
@@ -5,8 +5,12 @@ import SalaireNet from 'src/components/SalaryCalculation/SalaireNet'
 import SalaireNetAPayer from 'src/components/SalaryCalculation/SalaireNetAPayer'
 import PrimeEtAvantage from './PrimeEtAvantage'
 import { DocumentTextIcon } from '@heroicons/react/24/outline'
+import { useSelector } from 'react-redux'
 
 const SalaryCalculation = () => {
+  const selecteEmploy = useSelector((state) => state.selectedEmploye.employe)
+  const id = selecteEmploy.id
+  console.log(id)
   return (
     <>
       <div className="d-flex flex-column gap-4">
@@ -19,7 +23,7 @@ const SalaryCalculation = () => {
           <SalaireNetAPayer />
         </div>
         <div className="flex justify-end fixed top-[25%] p-1 right-0">
-          <Link to="/bulletin-de-paie" className="btn btn-primary">
+          <Link to={`/bulletin-de-paie/${id}`} className="btn btn-primary">
             <span className="flex items-center group">
               <DocumentTextIcon className="w-6 h-6" />
               <span className="ml-2 hidden group-hover:flex">Voir bulletin</span>
