d5868f29784fff515fe6f66c11a1ca178a4b2c1e
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-11 07:29:26 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,14 +8,18 @@ const corsOptions = {
   origin: [
     'https://3000-mampionona33-ittpsirh-w8k12dobh7e.ws-eu106.gitpod.io',
     'http://localhost:3000',
+    'https://rv8tjn-8000.csb.app',
   ],
   credentials: true,
-  optionsSuccessStatus: 200, // Fix the typo here
+  optionsSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions))
 
 app.get('/employees/list', (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*') // Allow any origin during development
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+
   res.status(200).send(db['/employees'])
 })
 
@@ -24,8 +28,12 @@ app.get('/employees/id=:id', (req, res) => {
   const employee = db['/employees'].find((emp) => emp.id === parseInt(id))
 
   if (employee) {
+    res.header('Access-Control-Allow-Origin', '*') // Allow any origin during development
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.status(200).json(employee)
   } else {
+    res.header('Access-Control-Allow-Origin', '*') // Allow any origin during development
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.status(404).send('Employee not found')
   }
 })
