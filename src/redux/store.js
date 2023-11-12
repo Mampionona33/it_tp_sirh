85bf04379f9efe86badf4d4fbcba8cd5ff012fb7
Mampionona33 <ramamps33@gmail.com>
2023-11-12 09:09:36 +0300

    ajout bulletin de paie au store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import sidebarReducer from './sidebar/sidebarReducer'
 import employeHoursReducer from './employeHours/employeHoursReducer'
 import selectedEmployeReducer from './selectedEmploye/selectedEmployeReducer'
+import bulletinDePaieReducer from './bulletinDePaie/bulletinDePaieReducer'
 
 const rootReducer = combineReducers({
   sidebar: sidebarReducer,
   employeHours: employeHoursReducer,
   selectedEmploye: selectedEmployeReducer,
+  bulletinDePaie: bulletinDePaieReducer,
 })
 
 const configStore = configureStore({
