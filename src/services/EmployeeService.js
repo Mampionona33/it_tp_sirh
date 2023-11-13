e2a229de3a8a24d00f70253dcfe26dbaea28350d
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-13 11:56:56 +0000

    Update EmployeeService.js

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -4,8 +4,18 @@ const REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://loc
 console.log(process.env.REACT_APP_API_BASE_URL)
 
 class EmployeeService {
-  getAll() {
-    return axios.get(`${REACT_APP_API_BASE_URL}/employees/list`)
+  async getAll() {
+    try {
+      const response = await axios.post(`${REACT_APP_API_BASE_URL}`, {
+        matricule: 345,
+        dateDebut: '02/01/2023',
+        dateFin: '06/01/2023',
+      })
+      console.log(response.data)
+      return response.data
+    } catch (error) {
+      console.error("Une erreur s'est produite lors de la requête :", error)
+    }
   }
   getById(id) {
     return axios.get(`${REACT_APP_API_BASE_URL}/employees/id=${id}`)
