810ac64d3c2b1f1de84b6b5fdda7691bf7dbd606
Mampionona33 <ramamps33@gmail.com>
2023-11-17 00:16:36 +0300

    comment useEffect

--- src/components/SalaryCalculation/SalaireBrut.jsx
+++ src/components/SalaryCalculation/SalaireBrut.jsx
@@ -185,118 +185,84 @@ const SalaireBrut = () => {
     )
   }
 
-  React.useEffect(() => {
-    let mount = true
-    if (salaireBrute_ && mount) {
-      // dispatch(setSelectedEmployeSalaireBrut(salaireBrute_))
-      dispatch(setBulletinDePaie({ salaireBrute: salaireBrute_ }))
-    }
-    if (hsni130_) {
-      if (mount) {
-        dispatch(setBulletinDePaie({ hsni130: hsni130_ }))
-      }
-    }
-    if (hsni150_ && mount) {
-      dispatch(setBulletinDePaie({ hsni150_: hsni150_ }))
-    }
+  // React.useEffect(() => {
+  //   let mount = true
+  //   if (salaireBrute_ && mount) {
+  //     // dispatch(setSelectedEmployeSalaireBrut(salaireBrute_))
+  //     dispatch(setBulletinDePaie({ salaireBrute: salaireBrute_ }))
+  //   }
+  //   if (hsni130_) {
+  //     if (mount) {
+  //       dispatch(setBulletinDePaie({ hsni130: hsni130_ }))
+  //     }
+  //   }
+  //   if (hsni150_ && mount) {
+  //     dispatch(setBulletinDePaie({ hsni150_: hsni150_ }))
+  //   }
 
-    if (hn30 && mount) {
-      dispatch(setBulletinDePaie({ hs30: hn30 }))
-    }
-    if (hn50 && mount) {
-      dispatch(setBulletinDePaie({ hs50: hn50 }))
-    }
+  //   if (hn30 && mount) {
+  //     dispatch(setBulletinDePaie({ hs30: hn30 }))
+  //   }
+  //   if (hn50 && mount) {
+  //     dispatch(setBulletinDePaie({ hs50: hn50 }))
+  //   }
 
-    if (baseIrsa && mount) {
-      dispatch(setBulletinDePaie({ baseIrsa: baseIrsa }))
-    }
-    if (omsi_ && mount) {
-      dispatch(setBulletinDePaie({ omsi: omsi_ }))
-    }
+  //   if (baseIrsa && mount) {
+  //     dispatch(setBulletinDePaie({ baseIrsa: baseIrsa }))
+  //   }
+  //   if (omsi_ && mount) {
+  //     dispatch(setBulletinDePaie({ omsi: omsi_ }))
+  //   }
 
-    if (plafondSME && mount) {
-      dispatch(setBulletinDePaie({ plafondSME: plafondSME }))
-    }
+  //   if (plafondSME && mount) {
+  //     dispatch(setBulletinDePaie({ plafondSME: plafondSME }))
+  //   }
 
-    if (tauxCnaps && mount && cnaps_ && baseCnaps) {
-      const cnapsObjectIndex = retenuSalaire.findIndex((ret) => ret.label === 'cnaps')
+  //   if (tauxCnaps && mount && cnaps_ && baseCnaps) {
+  //     const cnapsObjectIndex = retenuSalaire.findIndex((ret) => ret.label === 'cnaps')
 
-      if (cnapsObjectIndex === -1) {
-        // Si l'objet 'cnaps' n'existe pas encore dans le tableau, ajoutez-le
-        dispatch(
-          setBulletinDePaie({
-            retenuSalaire: [
-              ...retenuSalaire,
-              { label: 'cnaps', base: baseCnaps, taux: tauxCnaps, montant: cnaps_ },
-            ],
-          }),
-        )
-      } else {
-        // Si l'objet 'cnaps' existe déjà, mettez à jour ses propriétés
-        dispatch(
-          setBulletinDePaie({
-            retenuSalaire: retenuSalaire.map((ret, index) =>
-              index === cnapsObjectIndex
-                ? { ...ret, base: baseCnaps, taux: tauxCnaps, montant: cnaps_ }
-                : ret,
-            ),
-          }),
-        )
-      }
-    }
+  //     if (cnapsObjectIndex === -1) {
+  //       // Si l'objet 'cnaps' n'existe pas encore dans le tableau, ajoutez-le
+  //       dispatch(
+  //         setBulletinDePaie({
+  //           retenuSalaire: [
+  //             ...retenuSalaire,
+  //             { label: 'cnaps', base: baseCnaps, taux: tauxCnaps, montant: cnaps_ },
+  //           ],
+  //         }),
+  //       )
+  //     } else {
+  //       // Si l'objet 'cnaps' existe déjà, mettez à jour ses propriétés
+  //       dispatch(
+  //         setBulletinDePaie({
+  //           retenuSalaire: retenuSalaire.map((ret, index) =>
+  //             index === cnapsObjectIndex
+  //               ? { ...ret, base: baseCnaps, taux: tauxCnaps, montant: cnaps_ }
+  //               : ret,
+  //           ),
+  //         }),
+  //       )
+  //     }
+  //   }
 
-    return () => {
-      mount = false
-    }
-  }, [
-    salaireBrute_,
-    tauxCnaps,
-    tauxOmsi,
-    hsni130_,
-    plafondSME,
-    hsni150_,
-    hn30,
-    hn50,
-    dispatch,
-    cnaps_,
-    baseIrsa,
-    baseCnaps,
-    omsi_,
-  ])
-
-  useEffect(() => {
-    let mount = true
-    if (baseIrsa && mount && salaireBrute_ && omsi_ && cnaps_ && hsni130_ && hsi150_) {
-      const irsaObject = retenuSalaire.findIndex((ret) => ret.label === 'irsa')
-
-      if (irsaObject === -1) {
-        // Si l'objet 'cnaps' n'existe pas encore dans le tableau, ajoutez-le
-        dispatch(
-          setBulletinDePaie({
-            retenuSalaire: [...retenuSalaire, { label: 'irsa', montant: baseIrsa }],
-          }),
-        )
-      } else {
-        // Si l'objet 'cnaps' existe déjà, mettez à jour ses propriétés
-        dispatch(
-          setBulletinDePaie({
-            retenuSalaire: retenuSalaire.map((ret, index) =>
-              index === irsaObject ? { ...ret, montant: baseIrsa } : ret,
-            ),
-          }),
-        )
-      }
-    }
-
-    return () => {
-      mount = false
-    }
-  }, [dispatch, baseIrsa, retenuSalaire, cnaps_, omsi_, hsni130_, hsni150_, salaireBrute_])
-
-  // useEffect(() => {
-  //   console.log('baseIrsa:', baseIrsa)
-  //   // ... rest of the code
-  // }, [dispatch, baseIrsa, retenuSalaire])
+  //   return () => {
+  //     mount = false
+  //   }
+  // }, [
+  //   salaireBrute_,
+  //   tauxCnaps,
+  //   tauxOmsi,
+  //   hsni130_,
+  //   plafondSME,
+  //   hsni150_,
+  //   hn30,
+  //   hn50,
+  //   dispatch,
+  //   cnaps_,
+  //   baseIrsa,
+  //   baseCnaps,
+  //   omsi_,
+  // ])
 
   return (
     <div>
