d58ac78be30f7515d8673c051be300158e2f7656
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-17 09:20:12 +0000

    Update 3 files

--- src/components/SalaryCalculation/SalaireBrut.jsx
+++ src/components/SalaryCalculation/SalaireBrut.jsx
@@ -23,6 +23,11 @@ const SalaireBrut = () => {
   const salaireDeBase = useSelector((state) => state.bulletinDePaie.salaireDeBase)
   const hsni130 = useSelector((state) => state.bulletinDePaie.hsni130)
   const hsni150 = useSelector((state) => state.bulletinDePaie.hsni150)
+  const totalHs130 = useSelector((state) => state.bulletinDePaie.totalHs130)
+  const totalHs150 = useSelector((state) => state.bulletinDePaie.totalHs150)
+  const totalHs30 = useSelector((state) => state.bulletinDePaie.totalHs30)
+  const totalHs50 = useSelector((state) => state.bulletinDePaie.totalHs50)
+  const totalHDim = useSelector((state) => state.bulletinDePaie.totalHDim)
 
   const totalAjoutSalaire =
     ajoutSalaire.length > 0 &&
@@ -52,9 +57,13 @@ const SalaireBrut = () => {
     calc.setTauxHoraire(173.33)
     calc.setHsni130(hsni130)
     calc.setHsni150(hsni150)
-
+    calc.setTotalHs130(totalHs130)
+    calc.setTotalHs150(totalHs150)
+    calc.setTotalHn30(totalHs30)
+    calc.setTotalHn50(totalHs50)
+    calc.setTotalHDim(totalHDim)
     return calc
-  }, [salaireDeBase, hsni130, hsni150])
+  }, [salaireDeBase, hsni130, hsni150, totalHDim, totalHs130, totalHs150, totalHs30, totalHs50])
 
   // const calculPaie = new CalculPai(salaireDeBase)
   // calculPaie.setTauxHoraire(173.33)
@@ -69,98 +78,97 @@ const SalaireBrut = () => {
   // calculPaie.setTotalAjoutSalaire(totalAjoutSalaire ? totalAjoutSalaire : 0)
   // calculPaie.setTotalRetenuSalarie(totalRetenuSalarie ? totalRetenuSalarie : 0)
 
-  // const hsni130_ = calculPaie.getHsni130()
-  // const hsni150_ = calculPaie.getHsni150()
-  // const hsi130_ = calculPaie.getHsi130()
-  // const hsi150_ = calculPaie.getHsi150()
-  // const hn30 = calculPaie.getHn30()
-  // const hn50 = calculPaie.getHn50()
-  // const hdim = calculPaie.getHDim()
-  // const salaireBrute_ = calculPaie.getSalaireBrut()
-  // const cnaps_ = calculPaie.getCnaps()
-  // const omsi_ = calculPaie.getOmsi()
+  const hsni130_ = calculPaie.getHsni130()
+  const hsni150_ = calculPaie.getHsni150()
+  const hsi130_ = calculPaie.getHsi130()
+  const hsi150_ = calculPaie.getHsi150()
+  const hn30 = calculPaie.getHn30()
+  const hn50 = calculPaie.getHn50()
+  const hdim = calculPaie.getHDim()
+  const salaireBrute_ = calculPaie.getSalaireBrut()
+  const cnaps_ = calculPaie.getCnaps()
+  const omsi_ = calculPaie.getOmsi()
 
-  // const baseIrsa = useMemo(() => {
-  //   return calculPaie.getBaseIrsa()
-  // }, [calculPaie])
+  const baseIrsa = useMemo(() => {
+    return calculPaie.getBaseIrsa()
+  }, [calculPaie])
 
-  // const baseCnaps = useMemo(() => {
-  //   return calculPaie.getBaseCnaps()
-  // }, [calculPaie])
+  const baseCnaps = useMemo(() => {
+    return calculPaie.getBaseCnaps()
+  }, [calculPaie])
 
-  // const plafondSME = calculPaie.getPlafondSME()
-  // const tauxCnaps = calculPaie.getTauxCnaps()
-  // const tauxOmsi = calculPaie.getTauxOmsi()
+  const plafondSME = calculPaie.getPlafondSME()
+  const tauxCnaps = calculPaie.getTauxCnaps()
+  const tauxOmsi = calculPaie.getTauxOmsi()
 
-  // console.log(`hsni130_: ${hsni130_}`)
-  // console.log(`hsni150_: ${hsni150_}`)
-  // console.log(`hsi130_: ${hsi130_}`)
-  // console.log(`hsi150_: ${hsi150_}`)
-  // console.log(`hn30: ${hn30}`)
-  // console.log(`hn50: ${hn50}`)
-  // console.log(`hdim: ${hdim}`)
-  // console.log(`salaireBrute_: ${salaireBrute_}`)
-  // console.log(`cnaps_: ${cnaps_}`)
-  // console.log(`omsi_: ${omsi_}`)
-  // console.log(`baseIrsa: ${baseIrsa}`)
-  // console.log(`baseCnaps: ${baseCnaps}`)
+  console.log(`hsni130_: ${hsni130_}`)
+  console.log(`hsni150_: ${hsni150_}`)
+  console.log(`hsi130_: ${hsi130_}`)
+  console.log(`hsi150_: ${hsi150_}`)
+  console.log(`hn30: ${hn30}`)
+  console.log(`hn50: ${hn50}`)
+  console.log(`hdim: ${hdim}`)
+  console.log(`salaireBrute_: ${salaireBrute_}`)
+  console.log(`cnaps_: ${cnaps_}`)
+  console.log(`omsi_: ${omsi_}`)
+  console.log(`baseIrsa: ${baseIrsa}`)
+  console.log(`baseCnaps: ${baseCnaps}`)
   // console.log(indemnite)
   // ------------------------------
 
-  // const formatedHsni130Value = formatAriaryMga(hsni130_)
-  // const formatedHsni150Value = formatAriaryMga(hsni150_)
-  // const formatedHsi130Value = formatAriaryMga(hsi130_)
-  // const formatedHsi150Value = formatAriaryMga(hsi150_)
-  // const formatedHn30Value = formatAriaryMga(hn30)
-  // const formatedHn50Value = formatAriaryMga(hn50)
-  // const formatedHdimValue = formatAriaryMga(hdim)
-  // const formatedSlaireBruteValue = formatAriaryMga(salaireBrute_)
-  // const formatedSalaireBase = formatAriaryMga(salaireDeBase)
-  // const formatedPrimeEtAvantage = formatAriaryMga(totalAjoutSalaire)
+  const formatedHsni130Value = formatAriaryMga(hsni130_)
+  const formatedHsni150Value = formatAriaryMga(hsni150_)
+  const formatedHsi130Value = formatAriaryMga(hsi130_)
+  const formatedHsi150Value = formatAriaryMga(hsi150_)
+  const formatedHn30Value = formatAriaryMga(hn30)
+  const formatedHn50Value = formatAriaryMga(hn50)
+  const formatedHdimValue = formatAriaryMga(hdim)
+  const formatedSlaireBruteValue = formatAriaryMga(salaireBrute_)
+  const formatedSalaireBase = formatAriaryMga(salaireDeBase)
+  const formatedPrimeEtAvantage = formatAriaryMga(totalAjoutSalaire)
 
-  // const data = [
-  //   {
-  //     title: 'HSNI 130% :',
-  //     hours: `${employeeTotalHours.hsni130}`,
-  //     value: `${formatedHsni130Value}`,
-  //   },
-  //   {
-  //     title: 'HSNI 150% :',
-  //     hours: `${employeeTotalHours.hsni150}`,
-  //     value: `${formatedHsni150Value}`,
-  //   },
-  //   {
-  //     title: 'HSI 130% :',
-  //     hours: `${employeeTotalHours.totalHs130 - employeeTotalHours.hsni130}`,
-  //     value: `${formatedHsi130Value}`,
-  //   },
-  //   {
-  //     title: 'HSI 150% :',
-  //     hours: `${employeeTotalHours.totalHs150 - employeeTotalHours.hsni150}`,
-  //     value: `${formatedHsi150Value}`,
-  //   },
-  //   {
-  //     title: 'HN 30% :',
-  //     hours: `${employeeTotalHours.totalHs30}`,
-  //     value: `${formatedHn30Value}`,
-  //   },
-  //   {
-  //     title: 'HN 50% :',
-  //     hours: `${employeeTotalHours.totalHs50}`,
-  //     value: `${formatedHn50Value}`,
-  //   },
-  //   {
-  //     title: 'Hdim% :',
-  //     hours: `${employeeTotalHours.totalHdim}`,
-  //     value: `${formatedHdimValue}`,
-  //   },
-  // ]
+  const data = [
+    {
+      title: 'HSNI 130% :',
+      hours: `${employeeTotalHours.hsni130}`,
+      value: `${formatedHsni130Value}`,
+    },
+    {
+      title: 'HSNI 150% :',
+      hours: `${employeeTotalHours.hsni150}`,
+      value: `${formatedHsni150Value}`,
+    },
+    {
+      title: 'HSI 130% :',
+      hours: `${employeeTotalHours.totalHs130 - employeeTotalHours.hsni130}`,
+      value: `${formatedHsi130Value}`,
+    },
+    {
+      title: 'HSI 150% :',
+      hours: `${employeeTotalHours.totalHs150 - employeeTotalHours.hsni150}`,
+      value: `${formatedHsi150Value}`,
+    },
+    {
+      title: 'HN 30% :',
+      hours: `${employeeTotalHours.totalHs30}`,
+      value: `${formatedHn30Value}`,
+    },
+    {
+      title: 'HN 50% :',
+      hours: `${employeeTotalHours.totalHs50}`,
+      value: `${formatedHn50Value}`,
+    },
+    {
+      title: 'Hdim% :',
+      hours: `${employeeTotalHours.totalHdim}`,
+      value: `${formatedHdimValue}`,
+    },
+  ]
 
   const Body = () => {
     return (
       <>
-        <div>Salaire brut</div>
-        {/* <table className="table-auto">
+        <table className="table-auto">
           <tbody>
             <tr className="flex flex-wrap justify-between border-b border-customRed-100">
               <td colSpan="2" className="text-left py-3 pl-4 font-medium">
@@ -198,7 +206,7 @@ const SalaireBrut = () => {
               </td>
             </tr>
           </tbody>
-        </table> */}
+        </table>
       </>
     )
   }
