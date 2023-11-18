607fd3b243646011f9f5aa2cea7fc3db2fc80f9f
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-18 10:06:30 +0000

    Create HeureService.js

---
+++ src/services/HeureService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+class HeureService {
+  REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'
+  REACT_APP_API_BASE_URL_DEV = process.env.REACT_APP_API_BASE_URL_DEV || 'http://localhost:8000'
+
+  async getAll() {
+    try {
+      const resp = await axios.get(`${this.REACT_APP_API_BASE_URL}/heuressupplementaires`)
+      console.log(resp.data)
+      return resp.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+const newHeureService = new HeureService()
+export default newHeureService
