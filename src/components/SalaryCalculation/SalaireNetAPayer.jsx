af73aee72271c3dbd5b8891b883340e251f1de58
Mampionona33 <ramamps33@gmail.com>
2023-11-18 05:56:15 +0300

    changement salaire brute en salaire net

--- src/components/SalaryCalculation/SalaireNetAPayer.jsx
+++ src/components/SalaryCalculation/SalaireNetAPayer.jsx
@@ -2,37 +2,31 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import CustomSection from 'src/components/CustomSection'
 import formatAriaryMga from 'src/utils/formatAriaryMga'
-import { cotisastions } from 'src/db/db'
 
 const SalaireNetAPayer = () => {
   const title = 'Salaire net à payer'
-  const salaireBrut = useSelector((state) => state.selectedEmploye.salaireBrut)
+  const salaireNet = useSelector((state) => state.bulletinDePaie.salaireNet)
   const irsaValue = useSelector((state) => state.selectedEmploye.irsaValue)
-  const primeEtAvantage = useSelector((state) => state.selectedEmploye.primeEtAvantage)
   // // const retenue = useSelector((state) => state.bulletinDePaie.retenue)
   // const cnaps = retenue.find((ret) => ret.label === 'cnaps')
   // const ostie = retenue.find((ret) => ret.label === 'ostie')
   // const cnapsBase = cnaps && cnaps.base
   // const ostieBase = ostie && ostie.base
 
-  // const salaireNetAPayer = salaireBrut + irsaValue + cnapsBase + ostieBase + (primeEtAvantage || 0)
+  // const salaireNetAPayer = salaireNet + irsaValue + cnapsBase + ostieBase + (primeEtAvantage || 0)
 
   const data = [
     {
-      title: 'Salaire brut :',
-      value: `${formatAriaryMga(salaireBrut)}`,
+      title: 'Salaire net',
+      value: `${formatAriaryMga(salaireNet)}`,
     },
     {
-      title: 'CNAPS :',
+      title: 'Avance',
       // value: `${formatAriaryMga(cnapsBase)}`,
     },
     {
-      title: 'OSTIE :',
+      title: 'Allocation familiale',
       // value: `${formatAriaryMga(ostieBase)}`,
-    },
-    {
-      title: 'IRSA :',
-      value: `${formatAriaryMga(irsaValue)}`,
     },
     {
       title: 'Salaire net à payer :',
