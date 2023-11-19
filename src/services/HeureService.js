34e00d9012a9f1e6ff9c151a9e85ae03e9cf1665
Mampionona33 <ramamps33@gmail.com>
2023-11-19 16:54:55 +0300

    change HeuresService

--- src/services/HeureService.js
+++ src/services/HeureService.js
@@ -3,10 +3,20 @@ import axios from 'axios'
 class HeureService {
   REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'
   REACT_APP_API_BASE_URL_DEV = process.env.REACT_APP_API_BASE_URL_DEV || 'http://localhost:8000'
+  matricule = null
+  dateDebut = null
+  dateFin = null
+  async getAll(matricule, dateDebut, dateFin) {
+    this.matricule = matricule
+    this.dateDebut = dateDebut
+    this.dateFin = dateFin
 
-  async getAll() {
     try {
-      const resp = await axios.get(`${this.REACT_APP_API_BASE_URL}/heuressupplementaires`)
+      const resp = await axios.post(`${this.REACT_APP_API_BASE_URL}/heuressupplementaires`, {
+        matricule: matricule,
+        dateDebut: dateDebut,
+        dateFin: dateFin,
+      })
       console.log(resp.data)
       return resp.data
     } catch (error) {
