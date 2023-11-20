ffd619356d13f3bd1feb93797a702a735c0ece09
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-20 10:37:38 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Suspense } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
+import isAuthenticated from './auth'
 
 import { Provider } from 'react-redux'
 import './scss/style.scss'
@@ -22,13 +23,21 @@ const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
 const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.pathname = window.location.pathname
+    console.log(this.pathname)
+  }
   render() {
     return (
       <BrowserRouter>
         <Provider store={store}>
           <Suspense fallback={loading}>
             <Routes>
-              <Route path="/login" element={<Login />} />
+              <Route
+                path="/login"
+                element={isAuthenticated() ? <Navigate to="/dashboard" replace /> : <Login />}
+              />
               <Route path="/register" element={<Register />} />
               <Route exact path="/404" name="Page 404" element={<Page404 />} />
               <Route exact path="/500" name="Page 500" element={<Page500 />} />
