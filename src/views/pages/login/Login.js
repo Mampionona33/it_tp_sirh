c6c85265fb59752f8f39808f2ca1fba877c84c0f
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2023-11-23 07:02:19 +0000

    Update Login.js

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -100,8 +100,8 @@ const Login = () => {
                   <div>
                     <h2>Sign up</h2>
                     <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
+                      {`Intanet de gestion des paies, et des déclarations administratives
+                      (CNAPS, OSIE, Impôts)`}
                     </p>
                     <Link to="/register">
                       <CButton color="primary" className="mt-3" active tabIndex={-1}>
