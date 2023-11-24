3d1efee871a17cbe1597f13ef1ae4cad1cebb755
Mampionona33 <ramamps33@gmail.com>
2023-11-24 07:26:00 +0000

    hano ny

--- src/services/HeureService.js
+++ src/services/HeureService.js
@@ -3,8 +3,6 @@ import axios from 'axios'
 class HeureService {
   constructor() {
     this.REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'
-    this.REACT_APP_API_BASE_URL_DEV =
-      process.env.REACT_APP_API_BASE_URL_DEV || 'http://localhost:8000'
     this.matricule = null
     this.dateDebut = null
     this.dateFin = null
@@ -21,7 +19,7 @@ class HeureService {
         dateDebut: dateDebut,
         dateFin: dateFin,
       })
-      console.log(this.resp.data)
+      // console.log(this.resp.data)
       return this.resp.data
     } catch (error) {
       console.log(error)
