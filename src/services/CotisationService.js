368e3fc003d87820fec79747fa17a8aaed1ee59a
Mampionona33 <ramamps33@gmail.com>
2023-11-24 09:34:17 +0000

    push

--- src/services/CotisationService.js
+++ src/services/CotisationService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 class CotisationService {
   REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'
-
+  REACT_APP_API_BASE_URL_DEV = process.env.REACT_APP_API_BASE_URL_DEV || 'http://localhost:8000'
   async getAll() {
     try {
       const resp = await axios.get(`${this.REACT_APP_API_BASE_URL_DEV}/cotisations/all`)
