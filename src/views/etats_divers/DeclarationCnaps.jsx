43be634b9242c3bcdabfca1d87e85f694856036e
Mampionona33 <ramamps33@gmail.com>
2023-12-03 22:52:12 +0300

    try to setup downoload

--- src/views/etats_divers/DeclarationCnaps.jsx
+++ src/views/etats_divers/DeclarationCnaps.jsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react'
 import CustomSection from '../../components/CustomSection'
 import Select from 'react-select'
 import { selectCustomStyles } from 'src/scss/selectCustomStyles'
+import DnsGenerator from './DnsGenerator'
 
 const DeclarationCnaps = () => {
   const Body = () => {
     const [periode, setPeriode] = useState('t1')
     const [annee, setAnnee] = useState(new Date().getFullYear()) // Année actuelle
+    const [genererClicked, setGenererClicked] = useState(false)
+
     const periodesOptions = [
       { value: 't1', label: 'Trimestre 1' },
       { value: 't2', label: 'Trimestre 2' },
@@ -28,55 +31,69 @@ const DeclarationCnaps = () => {
       }
     }
 
-    const handleSubmit = (ev) => {
+    const handleSubmit = async (ev) => {
       ev.preventDefault()
-      console.log('submit')
-      // Ajoutez la logique pour traiter la soumission du formulaire
+      setGenererClicked(true)
+    }
+
+    const handleReset = () => {
+      setGenererClicked(false)
     }
 
     return (
-      <form className="w-full flex flex-col gap-2 p-4" onSubmit={handleSubmit}>
-        <div className="w-full flex flex-row gap-2 justify-between flex-wrap">
-          <div className="w-1/2">
-            <label className="form-label" htmlFor="periode">
-              Période
-            </label>
-            <Select
-              className="basic-multi-select"
-              name="periode"
-              options={periodesOptions}
-              onChange={(selectedOption, actionMeta) =>
-                handleInputChange(selectedOption, actionMeta)
-              }
-              value={periodesOptions.find((option) => option.value === periode)}
-              styles={selectCustomStyles}
-            />
+      <>
+        <form className="w-full flex flex-col gap-2 p-4" onSubmit={handleSubmit}>
+          <div className="w-full flex flex-row gap-2 justify-between flex-wrap">
+            <div className="w-1/2">
+              <label className="form-label" htmlFor="periode">
+                Période
+              </label>
+              <Select
+                className="basic-multi-select"
+                name="periode"
+                options={periodesOptions}
+                onChange={(selectedOption, actionMeta) =>
+                  handleInputChange(selectedOption, actionMeta)
+                }
+                value={periodesOptions.find((option) => option.value === periode)}
+                styles={selectCustomStyles}
+              />
+            </div>
+            <div>
+              <label className="form-label" htmlFor="annee">
+                Année
+              </label>
+              <Select
+                className="basic-multi-select"
+                name="annee"
+                options={years}
+                onChange={(selectedOption, actionMeta) =>
+                  handleInputChange(selectedOption, actionMeta)
+                }
+                value={years.find((option) => option.value === annee)}
+                styles={selectCustomStyles}
+              />
+            </div>
+            <div className="flex items-end">
+              <button
+                type="submit"
+                className="bg-customRed-900 text-white hover:bg-customRed-200 py-2 px-3 hover:text-slate-200"
+              >
+                Générer
+              </button>
+            </div>
           </div>
+        </form>
+
+        {genererClicked && (
           <div>
-            <label className="form-label" htmlFor="annee">
-              Année
-            </label>
-            <Select
-              className="basic-multi-select"
-              name="annee"
-              options={years}
-              onChange={(selectedOption, actionMeta) =>
-                handleInputChange(selectedOption, actionMeta)
-              }
-              value={years.find((option) => option.value === annee)}
-              styles={selectCustomStyles}
-            />
-          </div>
-          <div className="flex items-end">
-            <button
-              type="submit"
-              className="bg-customRed-900 text-white hover:bg-customRed-200 py-2 px-3 hover:text-slate-200"
-            >
-              Générer
+            <DnsGenerator key={genererClicked.toString()} />
+            <button type="button" className="btn btn-secondary" onClick={handleReset}>
+              Réinitialiser
             </button>
           </div>
-        </div>
-      </form>
+        )}
+      </>
     )
   }
 
