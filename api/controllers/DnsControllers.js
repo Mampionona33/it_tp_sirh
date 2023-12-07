3453223b2ff465ef10901c64fe200655d25dc2f0
Mampionona33 <ramamps33@gmail.com>
2023-12-07 13:33:57 +0000

    setup dns

--- api/controllers/DnsControllers.js
+++ api/controllers/DnsControllers.js
@@ -1,11 +1,24 @@
 const db = require('../db/db.json')
 
 class DnsControllers {
-  constructor() {}
+  constructor() {
+
+  }
+  
+  isT1(req, res) {
+    req.periode = 't1' // 01; 02; 03 + annee = anne_poi
+  }
+  isT2(req, res) {
+    req.periode = 't2'// 04; 05; 06
+  }
+  isT3(req, res) {
+    req.periode = 't3'// 07; 08; 09
+  }
+
   fetchDns = (req, res) => {
     periode: req.periode
     annee: req.annee
 
-    
+    // find db["dns"] annee_mois suivant periode
   }
 }
