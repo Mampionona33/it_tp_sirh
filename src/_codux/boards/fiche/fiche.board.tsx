774ee613e99083fe6dbccf8a512dd09391134009
Mampionona33 <ramamps33@gmail.com>
2023-12-12 23:53:22 +0300

    remove inused file

---
+++ src/_codux/boards/fiche/fiche.board.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createBoard } from '@wixc3/react-board'
+import Fiche from '../../../views/employee/fiche/Fiche'
+
+export default createBoard({
+    name: 'fiche',
+    Board: () => (
+        <div>
+            <Fiche />
+        </div>
+    ),
+    isSnippet: true,
+    environmentProps: {
+        windowHeight: 495
+    }
+})
