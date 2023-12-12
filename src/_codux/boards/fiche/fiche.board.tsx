b96e0f6c714d9eb15efad71d5c4548ff493b1ab2
Mampionona33 <ramamps33@gmail.com>
2023-12-13 00:19:38 +0300

    change in package

--- src/_codux/boards/fiche/fiche.board.tsx
+++ src/_codux/boards/fiche/fiche.board.tsx
@@ -3,14 +3,14 @@ import { createBoard } from '@wixc3/react-board'
 import Fiche from '../../../views/employee/fiche/Fiche'
 
 export default createBoard({
-    name: 'fiche',
-    Board: () => (
-        <div>
-            <Fiche />
-        </div>
-    ),
-    isSnippet: true,
-    environmentProps: {
-        windowHeight: 495
-    }
+  name: 'fiche',
+  Board: () => (
+    <div>
+      <Fiche />
+    </div>
+  ),
+  isSnippet: true,
+  environmentProps: {
+    windowHeight: 495,
+  },
 })
