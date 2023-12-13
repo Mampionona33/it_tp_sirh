bb0d9aa9bdeb6a2658d4aa2233721b5f928b08f0
Mampionona33 <ramamps33@gmail.com>
2023-12-13 07:42:36 +0000

    finish

--- src/views/employee/fiche/Fiche.tsx
+++ src/views/employee/fiche/Fiche.tsx
@@ -1,118 +1,91 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useLocation, useNavigate, useParams } from 'react-router-dom'
-import FormInfoGalEmployee from '@src/components/FormInfoGalEmployee'
-import SalaryCalculation from '@src/components/SalaryCalculation/SalaryCalculation'
-import TimeSheetTable from '@src/components/TimeSheetTable/TimeSheetTable_'
-import { setBulletinDePaie } from '@src/redux/bulletinDePaie/bulletinDePaieReducer'
-
-interface RenderTabProps {
-  tab: { key: string; label: string }
-  activeTab: string
-  handleTabClick: (
-    event: React.MouseEvent<HTMLElement, MouseEvent> | string,
-  ) => void
-}
-
-const RenderTab: React.FC<RenderTabProps> = ({
-  tab,
-  activeTab,
-  handleTabClick,
-}) => (
-  <li key={tab.key} className="mr-2" role="presentation">
-    <button
-      className={`inline-block p-4 border-b-2 rounded-t-lg ${
-        activeTab === tab.key ? 'border-customRed-900' : ''
-      }`}
-      id={`${tab.key}-tab`}
-      data-tabs-target={`#${tab.key}`}
-      type="button"
-      role="tab"
-      aria-controls={tab.key}
-      aria-selected={activeTab === tab.key}
-      onClick={() => handleTabClick(tab.key)}
-    >
-      {tab.label}
-    </button>
-  </li>
-)
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useParams, useNavigate } from 'react-router-dom';
+import FormInfoGalEmployee from 'src/components/FormInfoGalEmployee';
+import SalaryCalculation from 'src/components/SalaryCalculation/SalaryCalculation';
+import TimeSheetTable from 'src/components/TimeSheetTable/TimeSheetTable_';
+import { setBulletinDePaie } from 'src/redux/bulletinDePaie/bulletinDePaieReducer';
 
 const Fiche: React.FC = () => {
-  const [activeTab, setActiveTab] = useState<string>('info-perso')
-  const { id, activeTabParam } = useParams()
-  const dispatch = useDispatch()
-  const employees: Array<any> = useSelector(
-    (state: any) => state.employeesList.list,
-  )
-  const location = useLocation()
-  const navigate = useNavigate()
-
-  const selectedEmployee: object = useSelector(
-    (state: any) => state.bulletinDePaie.salarie,
-  )
+  const [activeTab, setActiveTab] = useState<string>('info-perso');
+  const { id, activeTabParam } = useParams();
+  const dispatch = useDispatch();
+  const employees = useSelector((state: any) => state.employeesList.list);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const selectedEmployee = useSelector((state: any) => state.bulletinDePaie.salarie);
 
   useEffect(() => {
-    const pathParts = location.pathname.split('/')
-    const lastPathPart = pathParts[pathParts.length - 1]
+    const pathParts = location.pathname.split('/');
+    const lastPathPart = pathParts[pathParts.length - 1];
 
-    if (
-      ['info-perso', 'heures-travailles', 'bulletin-de-paie'].includes(
-        lastPathPart,
-      )
-    ) {
-      setActiveTab(lastPathPart)
+    if (['info-perso', 'heures-travailles', 'bulletin-de-paie'].includes(lastPathPart)) {
+      setActiveTab(lastPathPart);
     } else if (activeTabParam) {
-      setActiveTab(activeTabParam)
+      setActiveTab(activeTabParam);
     }
-  }, [location.pathname, activeTabParam])
+  }, [location.pathname, activeTabParam]);
+
+  const handleTabClick = (eventKey: string) => {
+    setActiveTab(eventKey);
+    navigate(`/employees/fiche/${id}/${eventKey}`);
+  };
+
+  const tabList = [
+    { key: 'info-perso', label: 'Information générale' },
+    { key: 'heures-travailles', label: 'Heures travaillées' },
+    { key: 'bulletin-de-paie', label: 'Calcul paie' },
+  ];
 
   useEffect(() => {
-    let mount = true
+    let mount = true;
 
     if (id && employees && mount) {
-      const emp = employees.find((empl) => empl.id === parseInt(id))
+      const emp = employees.find((empl: any) => empl.id === parseInt(id, 10));
 
       if (emp) {
-        const salaireBase = emp.salaireBase * 1
-        dispatch(setBulletinDePaie({ salarie: { ...emp } }))
-        dispatch(setBulletinDePaie({ salaireDeBase: salaireBase }))
+        const salaireBase = emp.salaireBase * 1;
+        dispatch(setBulletinDePaie({ salarie: { ...emp } }));
+        dispatch(setBulletinDePaie({ salaireDeBase: salaireBase }));
       }
     }
 
     return () => {
-      mount = false
-    }
-  }, [id, employees, dispatch])
+      mount = false;
+    };
+  }, [id, employees, dispatch]);
 
-  const tabList: Array<{ key: string; label: string }> = [
-    { key: 'info-perso', label: 'Information générale' },
-    { key: 'heures-travailles', label: 'Heures travaillées' },
-    { key: 'bulletin-de-paie', label: 'Calcul paie' },
-  ]
-
-  const handleTabClick = (
-    event: React.MouseEvent<HTMLElement, MouseEvent>,
-  ): void => {
-    const eventKey = event.currentTarget.getAttribute('data-key')
-
-    if (eventKey) {
-      setActiveTab(eventKey)
-      navigate(`/employees/fiche/${id}/${eventKey}`)
-    }
-  }
+  const renderTab = (tab: { key: string; label: string }) => (
+    <li key={tab.key} className="mr-2" role="presentation">
+      <button
+        className={`inline-block p-4 border-b-2 rounded-t-lg ${
+          activeTab === tab.key ? 'border-customRed-900' : ''
+        }`}
+        id={`${tab.key}-tab`}
+        data-tabs-target={`#${tab.key}`}
+        type="button"
+        role="tab"
+        aria-controls={tab.key}
+        aria-selected={activeTab === tab.key}
+        onClick={() => handleTabClick(tab.key)}
+      >
+        {tab.label}
+      </button>
+    </li>
+  );
 
   const renderTabContent = (tabKey: string): JSX.Element | null => {
     switch (tabKey) {
       case 'info-perso':
-        return <FormInfoGalEmployee />
+        return <FormInfoGalEmployee />;
       case 'heures-travailles':
-        return <TimeSheetTable />
+        return <TimeSheetTable />;
       case 'bulletin-de-paie':
-        return <SalaryCalculation />
+        return <SalaryCalculation />;
       default:
-        return null
+        return null;
     }
-  }
+  };
 
   return (
     <div className="bg-white dark:bg-gray-800 p-4 shadow">
@@ -122,14 +95,7 @@ const Fiche: React.FC = () => {
           id="myTab"
           role="tablist"
         >
-          {tabList.map((tab) => (
-            <RenderTab
-              key={tab.key}
-              tab={tab}
-              activeTab={activeTab}
-              handleTabClick={handleTabClick}
-            />
-          ))}
+          {tabList.map(renderTab)}
         </ul>
       </div>
       {/* Tab contents */}
@@ -149,7 +115,7 @@ const Fiche: React.FC = () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Fiche
+export default Fiche;
