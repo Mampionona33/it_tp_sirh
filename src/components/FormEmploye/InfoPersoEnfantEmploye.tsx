d1bd305a2caff319bcb03e6cb16938c48aaf9e40
Mampionona33 <ramamps33@gmail.com>
2023-12-18 13:27:54 +0000

    fisnish

--- src/components/FormEmploye/InfoPersoEnfantEmploye.tsx
+++ src/components/FormEmploye/InfoPersoEnfantEmploye.tsx
@@ -2,20 +2,33 @@ import React, { useState } from 'react'
 import { PlusIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import InputWithLabel, { IInputWithLabelProps } from './InputWithLable'
 
-const InfoPersoEnfantEmploye: React.FC = () => {
-  const [formData, setFormData] = useState({
+interface IFormEnfant {
+  nom: string
+  prenom: string
+  date_naissance: string
+  adresse: string
+  lieu_naissance: string
+  cin: string
+  genre: string
+}
+
+interface IFormEnfantsProps {
+  index: number
+}
+
+const FormEnfants: React.FC<IFormEnfantsProps> = ({ index }) => {
+  const [formData, setFormData] = useState<IFormEnfant>({
     nom: '',
     prenom: '',
     date_naissance: '',
+    adresse: '',
     lieu_naissance: '',
-    genre_enfant: '',
+    cin: '',
+    genre: '',
   })
 
-  const [nombreEnfant, setNombreEnfant] = useState(0)
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target
-    console.log(name, value, index)
 
     setFormData((prevData) => ({
       ...prevData,
@@ -23,90 +36,76 @@ const InfoPersoEnfantEmploye: React.FC = () => {
     }))
   }
 
-  const addEnfant = () => {
-    setNombreEnfant((prevNombreEnfant) => prevNombreEnfant + 1)
-    // creer l'input radio genre pour chaque enfant
-    // par exemple on vas push le inputs
-  }
-
-  const removeEnfant = (index: number) => {
-    setNombreEnfant((prevNombreEnfant) => prevNombreEnfant - 1)
-  }
-
-  const inputs: (IInputWithLabelProps | (() => IInputWithLabelProps))[] = [
-    {
-      label: 'Nom',
-      required: true,
-      name: 'nom',
-      type: 'text',
-      value: formData.nom,
-      onChange: (event) => handleInputChange(event, nombreEnfant),
-    },
-    {
-      label: 'Prénom',
-      required: true,
-      name: 'prenom',
-      type: 'text',
-      value: formData.prenom,
-      onChange: (event) => handleInputChange(event, nombreEnfant),
-    },
-    {
-      label: 'Date de naissance',
-      required: true,
-      name: 'date_naissance',
-      type: 'date',
-      value: formData.date_naissance,
-      onChange: (event) => handleInputChange(event, nombreEnfant),
-    },
-    {
-      label: 'Lieu de naissance',
-      required: true,
-      name: 'lieu_naissance',
-      type: 'text',
-      value: formData.lieu_naissance,
-      onChange: (event) => handleInputChange(event, nombreEnfant),
-    },
-  ]
+  const defaultGenre = index % 2 === 0 ? 'masculin' : 'feminin'
 
   return (
-    <div className="my-3">
-      <div className="flex justify-between ">
-        <button
-          onClick={addEnfant}
-          className="flex mx-3 bg-customRed-900 text-white px-3 py-1 items-center gap-2 shadow-sm hover:shadow-md"
-        >
-          <PlusIcon width={20} height={20} /> <span>Ajouter enfant</span>
-        </button>
+    <>
+      <div>
+        <label htmlFor={`nom_${index}`}>Nom</label>
+        <input type="text" name={`nom_${index}`} id={`nom_${index}`} onChange={handleInputChange} />
       </div>
-      {Array.from({ length: nombreEnfant }, (_, index) => (
-        <div key={index} className="flex bg-customRed-25 mt-2 relative">
-          <button
-            onClick={() => removeEnfant(index)}
-            className="flex bg-customRed-900 text-white p-1 items-center shadow-sm absolute right-1 top-1 hover:shadow-md"
-          >
-            <span>
-              <XMarkIcon width={20} height={20} />
-            </span>
-          </button>
-          <div className="grid grid-cols-3 gap-x-3 gap-y-0 px-8 py-3 w-full">
-            {inputs.map((input, inputIndex) =>
-              typeof input === 'function' ? (
-                <InputWithLabel
-                  key={inputIndex}
-                  {...input()}
-                  onChange={(event) => handleInputChange(event, index)}
-                />
-              ) : (
-                <InputWithLabel
-                  key={inputIndex}
-                  {...input}
-                  onChange={(event) => handleInputChange(event, inputIndex)}
-                />
-              ),
-            )}
-          </div>
-        </div>
-      ))}
+      <div>
+        <label htmlFor={`masculin_${index}`}>Masculin</label>
+        <input
+          type="radio"
+          name={`genre_${index}`}
+          id={`masculin_${index}`}
+          value="masculin"
+          checked={formData.genre === 'masculin' || formData.genre === ''}
+          onChange={(event) =>
+            setFormData((prevData) => ({ ...prevData, genre: event.target.value }))
+          }
+        />
+
+        <label htmlFor={`feminin_${index}`}>Féminin</label>
+        <input
+          type="radio"
+          name={`genre_${index}`}
+          id={`feminin_${index}`}
+          value="feminin"
+          checked={formData.genre === 'feminin'}
+          onChange={(event) =>
+            setFormData((prevData) => ({ ...prevData, genre: event.target.value }))
+          }
+        />
+      </div>
+    </>
+  )
+}
+
+const InfoPersoEnfantEmploye: React.FC = () => {
+  const [nombreEnfant, setNombreEnfant] = useState(0)
+  const [formData, setFormData] = useState({
+    enfants: Array.from({ length: nombreEnfant }, () => ({
+      nom: '',
+      prenom: '',
+      date_naissance: '',
+      lieu_naissance: '',
+      genre_enfant: '',
+    })),
+  })
+
+  const addEnfant = () => {
+    setNombreEnfant((prevNombreEnfant) => prevNombreEnfant + 1)
+    setFormData((prevData) => ({
+      ...prevData,
+      enfants: [
+        ...prevData.enfants,
+        { nom: '', prenom: '', date_naissance: '', lieu_naissance: '', genre_enfant: '' },
+      ],
+    }))
+  }
+
+  return (
+    <div>
+      <div>
+        <button className="bg-customRed-900 text-white flex items-center" onClick={addEnfant}>
+          <PlusIcon width={20} height={20} /> Ajouter un enfant
+        </button>
+        {formData.enfants.map((enfant, index) => (
+          <FormEnfants key={index} index={index} />
+        ))}
+      </div>
     </div>
   )
 }
