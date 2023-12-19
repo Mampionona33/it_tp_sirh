c0072b61d3b34f5b3149e96cd93ab9839477c8b6
Mampionona <50689325+Mampionona33@users.noreply.github.com>
2023-12-19 12:57:17 +0300

    changement parametre pretinrc

--- src/components/FormEmploye/InputWithLable.tsx
+++ src/components/FormEmploye/InputWithLable.tsx
@@ -2,123 +2,123 @@ import React, { ChangeEvent } from 'react'
 import Select from 'react-select'
 
 export interface IInputWithLabelProps {
-	label: string
-	name: string
-	type: string
-	required?: boolean
-	value?: any
-	index?: number
-	options?:
-		| { label: string; value: string }[]
-		| { label: string; value: string }[]
-		| { label: number; value: string }[]
-	onChange?: (value: any, index?: number) => void
+  label: string
+  name: string
+  type: string
+  required?: boolean
+  value?: any
+  index?: number
+  options?:
+    | { label: string; value: string }[]
+    | { label: string; value: string }[]
+    | { label: number; value: string }[]
+  onChange?: (value: any, index?: number) => void
 }
 
 const InputWithLabel: React.FC<IInputWithLabelProps> = ({
-	label,
-	name,
-	type,
-	required,
-	value,
-	onChange,
-	options,
-	index,
+  label,
+  name,
+  type,
+  required,
+  value,
+  onChange,
+  options,
+  index,
 }) => {
-	const customSelectStyles = {
-		control: (provided, state) => ({
-			...provided,
-			borderColor: state.isFocused ? '#da200d' : '',
-			boxShadow: state.isFocused ? '0 0 0 0.25rem #e7b7b4' : null,
-			borderRadius: 0,
-		}),
+  const customSelectStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      borderColor: state.isFocused ? '#da200d' : '',
+      boxShadow: state.isFocused ? '0 0 0 0.25rem #e7b7b4' : null,
+      borderRadius: 0,
+    }),
 
-		container: (provided) => ({
-			...provided,
-			height: '28px',
-		}),
-		// menu: (provided, state) => ({
-		//   ...provided,
-		//   width: "100%",
-		// }),
-		// option: (provided, state) => ({
-		//   ...provided,
-		//   backgroundColor: state.isSelected ? "#da200d" : "inherit",
-		//   ":hover": {
-		//     backgroundColor: "#e7b7b4",
-		//   },
-		// }),
-	}
+    container: (provided) => ({
+      ...provided,
+      height: '28px',
+    }),
+    // menu: (provided, state) => ({
+    //   ...provided,
+    //   width: "100%",
+    // }),
+    // option: (provided, state) => ({
+    //   ...provided,
+    //   backgroundColor: state.isSelected ? "#da200d" : "inherit",
+    //   ":hover": {
+    //     backgroundColor: "#e7b7b4",
+    //   },
+    // }),
+  }
 
-	return (
-		<div className="flex flex-col mb-2">
-			{type !== 'radio' && type !== 'select' && (
-				<label htmlFor={name}>
-					{label} {required ? '*' : ''}
-				</label>
-			)}
-			{type === 'radio' && options ? (
-				// Render a fieldset for radio buttons
-				<fieldset>
-					<legend className="text-base">{label}</legend>
-					{options.map((option, index) => (
-						<div key={index} className="grid grid-cols-4">
-							<div className="text-center">
-								<input
-									type="radio"
-									id={`${name}_${index}`}
-									name={name}
-									value={option.value}
-									checked={value === option.value}
-									onChange={(event) => onChange(event, index)}
-								/>
-							</div>
-							<label htmlFor={`${name}_${index}`} className="ml-1 col-span-3">
-								{option.label}
-							</label>
-						</div>
-					))}
-				</fieldset>
-			) : type === 'select' && options ? (
-				// Render a select dropdown using react-select
-				<div className="flex flex-col mb-2">
-					<label htmlFor={name}>{label}</label>
-					<Select
-						aria-label={label}
-						name={name}
-						options={options as { label: string; value: string }[]}
-						value={options.find(
-							(opt: { label: string; value: string }) => opt.value === value
-						)}
-						onChange={(
-							selectedOption: { label: string; value: string } | undefined
-						) => onChange(selectedOption ? selectedOption.value : '', index)}
-						theme={(theme) => ({
-							...theme,
-							borderRadius: 0,
-							height: 28,
-							colors: {
-								primary25: '#D6111E',
-							},
-						})}
-					/>
-				</div>
-			) : (
-				// Render a regular input for other types
-				<input
-					type={type}
-					name={name}
-					id={name}
-					value={value}
-					onChange={(event) =>
-						onChange(event as ChangeEvent<HTMLInputElement>, index)
-					}
-					className="border border-customRed-50 focus:outline-customRed-100 p-2 h-[28px]"
-					required={required}
-				/>
-			)}
-		</div>
-	)
+  return (
+    <div className="flex flex-col mb-2">
+      {type !== 'radio' && type !== 'select' && (
+        <label htmlFor={name}>
+          {label} {required ? '*' : ''}
+        </label>
+      )}
+      {type === 'radio' && options ? (
+        // Render a fieldset for radio buttons
+        <fieldset>
+          <legend className="text-base">{label}</legend>
+          {options.map((option, index) => (
+            <div key={index} className="grid grid-cols-4">
+              <div className="text-center">
+                <input
+                  type="radio"
+                  id={`${name}_${index}`}
+                  name={name}
+                  value={option.value}
+                  checked={value === option.value}
+                  onChange={(event) => onChange(event, index)}
+                />
+              </div>
+              <label htmlFor={`${name}_${index}`} className="ml-1 col-span-3">
+                {option.label}
+              </label>
+            </div>
+          ))}
+        </fieldset>
+      ) : type === 'select' && options ? (
+        // Render a select dropdown using react-select
+        <div className="flex flex-col mb-2">
+          <label htmlFor={name}>{label}</label>
+          <Select
+            aria-label={label}
+            name={name}
+            options={options as { label: string; value: string }[]}
+            value={options.find(
+              (opt: { label: string; value: string }) => opt.value === value
+            )}
+            onChange={(
+              selectedOption: { label: string; value: string } | undefined
+            ) => onChange(selectedOption ? selectedOption.value : '', index)}
+            theme={(theme) => ({
+              ...theme,
+              borderRadius: 0,
+              height: 28,
+              colors: {
+                primary25: '#D6111E',
+              },
+            })}
+          />
+        </div>
+      ) : (
+        // Render a regular input for other types
+        <input
+          type={type}
+          name={name}
+          id={name}
+          value={value}
+          onChange={(event) =>
+            onChange(event as ChangeEvent<HTMLInputElement>, index)
+          }
+          className="border border-customRed-50 focus:outline-customRed-100 p-2 h-[28px]"
+          required={required}
+        />
+      )}
+    </div>
+  )
 }
 
 export default InputWithLabel
