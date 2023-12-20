0f61537a88604dff98e4205c0a74e94d6b450f1e
Mampionona33 <ramamps33@gmail.com>
2023-12-20 19:44:24 +0300

    ajout commentair important

--- src/components/TableListeEmploye/TableListeEmploye.tsx
+++ src/components/TableListeEmploye/TableListeEmploye.tsx
@@ -9,7 +9,14 @@ interface IDataWithActions extends IEmploye {
 
 function TableListeEmploye({ actions }: { actions?: React.FC[] }): JSX.Element {
   const data = useSelector((state: any) => state.employeesList.list)
-
+  /**
+   * Pour assurer la stabilité des données dans la table,
+   * il est nécessaire d'isoler les données à envoyer en les formatant
+   * en dehors du composant TableEmployeeDataTable.
+   * @tanstack/react-table ne gère pas efficacement les changements fréquents d'état.
+   * Ainsi, les données sont formatées une fois et transmises en tant que propriétés (props)
+   * pour maintenir la constance des données à l'intérieur de la table.
+   */
   const formattedData: IDataWithActions[] =
     data && data.length > 0
       ? data.map((item: any) => ({
