e2fdd44b5d1d123b648123435031176cd96e9de0
Mampionona33 <ramamps33@gmail.com>
2023-12-27 04:37:27 +0300

    ajout reinitialisation form après click sur boutton annulé

--- src/components/FormEmploye/FormEmployeGroupButton.tsx
+++ src/components/FormEmploye/FormEmployeGroupButton.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import ButtonWithIcon, { ButtonWithIconVariant } from '../buttons/ButtonWithIcon'
 import ButtonLink, { ButtonLinkVariant } from '../buttons/ButtonLink'
+import { useAppDispatch } from '@src/hooks/useAppDispatch'
+import { resetFormEmploye } from '@src/redux/FormEmploye/formEmployeReducer'
 
 const FormEmployeGroupButton = () => {
+  const dispatch = useAppDispatch()
+
+  const handleclickCancel = () => {
+    dispatch(resetFormEmploye())
+  }
   return (
     <>
       <div className="flex flex-row flex-wrap justify-end m-4 gap-3">
-        <ButtonLink to="/employees/list" variant={ButtonLinkVariant.Tertiary}>
+        <ButtonLink
+          onClick={handleclickCancel}
+          to="/employees/list"
+          variant={ButtonLinkVariant.Tertiary}
+        >
           Annuler
         </ButtonLink>
         <ButtonWithIcon
