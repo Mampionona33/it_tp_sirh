498932e4297df6627d9b9edb499bcf56172ad33e
Mampionona33 <ramamps33@gmail.com>
2024-01-01 10:29:52 +0300

    mise en forme calcul salarie net à payer

--- src/views/gestion-paie/valider-paie/ValiderCalculPaie/CardSalaireNetAPayer.tsx
+++ src/views/gestion-paie/valider-paie/ValiderCalculPaie/CardSalaireNetAPayer.tsx
@@ -1,11 +1,33 @@
 import CustomSection from '@src/components/CustomSection'
 import React from 'react'
+import CardRow from './CardRow'
+import { useAppSelector } from '@src/hooks/useAppDispatch'
 
 const CardSalaireNetAPayer = () => {
+  const bulletinDePaie = useAppSelector((store) => store.bulletinDePaie)
   const Body = () => {
     return (
-      <div>
-        <p>Body</p>
+      <div className="w-full text-sm">
+        <CardRow
+          className="border-b border-b-customBlue-100"
+          cell1="Salaire Net"
+          cell3={bulletinDePaie.salaireNet}
+        />
+        <CardRow
+          className="border-b border-b-customBlue-100"
+          cell1="Avances"
+          cell3={bulletinDePaie.salaireNet}
+        />
+        <CardRow
+          className="border-b border-b-customBlue-100"
+          cell1="Avantage en nature"
+          cell3={bulletinDePaie.salaireNet}
+        />
+        <CardRow
+          className="border-b border-b-customBlue-100"
+          cell1="Allocation familialee"
+          cell3={bulletinDePaie.salaireNet}
+        />
       </div>
     )
   }
