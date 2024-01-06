18cf03aaca9c6b19df9408b3f6fd280ab72e9e22
Mampionona33 <ramamps33@gmail.com>
2024-01-06 15:37:34 +0300

    modification id bulletin de paie

--- api/src/controllers/BulletinDePaieControllers.ts
+++ api/src/controllers/BulletinDePaieControllers.ts
@@ -13,21 +13,21 @@ class BulletinDePaieControllers {
       const dbFilePath = path.join(__dirname, '../db/db.json')
 
       const dbContent = JSON.parse(fs.readFileSync(dbFilePath, 'utf-8'))
-      const id = dbContent.bulletinDePaie.length + 1
-      // Ajouter la nouvelle donnée au tableau dans le fichier db.json
-      dbContent.bulletinDePaie.push({ ...req.body, id })
 
-      // Écrire le contenu mis à jour dans le fichier db.json
+      const idBltnPaie = dbContent.bulletinDePaie.length + 1
+
+      dbContent.bulletinDePaie.push({ ...req.body, idBltnPaie })
+
       fs.writeFileSync(dbFilePath, JSON.stringify(dbContent, null, 2), 'utf-8')
 
-      // Répondre avec la nouvelle donnée
       res.status(201).json(req.body)
     } catch (error) {
-      // En cas d'erreur, répondre avec un statut d'erreur
       console.error("Une erreur s'est produite lors de l'écriture dans le fichier db.json :", error)
       res.status(500).send('Erreur serveur')
     }
   }
+
+  getAll = (req: Request, res: Response) => {}
 }
 
 export default BulletinDePaieControllers
