e350036dbfde5ab07ebc40fcac7c8f30faa5dccc
Mampionona33 <ramamps33@gmail.com>
2024-01-06 22:22:00 +0300

    start to fix date

--- api/src/controllers/HistoriquePaieController.ts
+++ api/src/controllers/HistoriquePaieController.ts
@@ -1,6 +1,14 @@
 import { Request, Response } from 'express'
 import { IBulletinDePaieProps } from 'interfaces/interfaceBulletinDePaie'
 
+export interface IGetOneProps {
+  id: number
+  id_employe?: number | string
+  date?: string
+  salaireBrut?: number
+  salaireNet?: number
+  status?: string
+}
 class HistoriquePaieController {
   private db: any
   constructor(db: Object) {
@@ -10,6 +18,7 @@ class HistoriquePaieController {
   getOne = (req: Request, res: Response) => {
     const { id, annee } = req.params
     const listBulletinDePaie: IBulletinDePaieProps[] = this.db['bulletinDePaie']
+    const resp: IGetOneProps[] = []
 
     const data = listBulletinDePaie.filter(
       (blt) =>
@@ -17,7 +26,18 @@ class HistoriquePaieController {
         new Date(blt.validation?.date ?? '').getFullYear() === parseInt(annee, 10),
     )
 
-    return res.status(200).send(data)
+    for (let i = 0; i < data.length; i++) {
+      resp.push({
+        id: i,
+        id_employe: data[i].salarie?.id,
+        date: data[i].validation?.date,
+        salaireBrut: data[i].salaireBrut,
+        salaireNet: data[i].salaireNet,
+        status: data[i].validation?.status,
+      })
+    }
+
+    return res.status(200).send(resp)
   }
 }
 
