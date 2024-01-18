ef9feb61e03eab63b4fad87be25ffa45d07717cc
Mampionona33 <ramamps33@gmail.com>
2024-01-19 02:52:16 +0300

    Creation methode getmois

---
+++ src/interfaces/interfaceDnsGenerator.ts
@@ -0,0 +1,93 @@
+export interface IDnsGeneratorRootProps {
+  data: IDnsGeneratorDataProps[]
+  status: number
+  statusText: string
+  headers?: IDnsHeadersProps
+  config?: IDnsConfigProps
+  request?: Request
+}
+
+export interface IDnsGeneratorDataProps {
+  travailleurs: Travailleur[]
+  employeur: Employeur[]
+}
+
+export interface Travailleur {
+  id: number
+  annee: string
+  trimestre: string
+  mois: string
+  matricule: string
+  nom: string
+  prenom: string
+  date_embauche: string
+  date_depart: any
+  salaire_de_base: number
+  avantage_du_mois: number
+  temps_presence: number
+  hs_non_plafonne: number
+  hs_plafonne: number
+  num_cnaps: string
+  cin: string
+  ref_employeur: string
+  created_at: any
+  taux_cotisation_cnaps_employeur: number
+  taux_cotisation_cnaps_salarie: number
+  updated_at: any
+}
+
+export interface Employeur {
+  id: number
+  nom: string
+  numero_rcs: string
+  adresse: string
+  cp_ville: string
+  numero_nif: string
+  numero_stat: string
+  email: string
+  telephone: string
+  created_at: any
+  updated_at: any
+}
+
+export interface IDnsHeadersProps {
+  'content-length': string
+  'content-type': string
+}
+
+export interface IDnsConfigProps {
+  transitional: Transitional
+  adapter: string[]
+  transformRequest: any[]
+  transformResponse: any[]
+  timeout: number
+  xsrfCookieName: string
+  xsrfHeaderName: string
+  maxContentLength: number
+  maxBodyLength: number
+  env: Env
+  headers: Headers2
+  auth: Auth
+  method: string
+  url: string
+}
+
+export interface Transitional {
+  silentJSONParsing: boolean
+  forcedJSONParsing: boolean
+  clarifyTimeoutError: boolean
+}
+
+export interface Env {}
+
+export interface Headers2 {
+  Accept: string
+  Authorization: string
+}
+
+export interface Auth {
+  username: string
+  password: string
+}
+
+export interface Request {}
