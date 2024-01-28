5edee56bf1d973ec355fdd97a4804a19d3fa5b8a
Mampionona33 <ramamps33@gmail.com>
2024-01-28 21:17:58 +0300

    créer service pour les taux cnaps

---
+++ src/services/TauxCnapsService.ts
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+class TauxCnapsService {
+  private REACT_APP_API_BASE_URL: string
+  private REACT_APP_API_BASE_URL_DEV: string
+  private login: string
+  private pass: string
+  constructor() {
+    this.REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'
+    this.REACT_APP_API_BASE_URL_DEV =
+      process.env.REACT_APP_API_BASE_URL_DEV || 'http://localhost:8000'
+    this.login = 'lslisteemployes'
+    this.pass = '20lsliste23'
+  }
+
+  public getTauxEmployeur = async () => {
+    try {
+      const response = await axios.get(`${this.REACT_APP_API_BASE_URL_DEV}/taux-cnaps/employeur`, {
+        auth: {
+          username: this.login,
+          password: this.pass,
+        },
+      })
+      return response
+    } catch (error) {
+      throw error
+    }
+  }
+
+  public getTauxSalarie = async () => {
+    try {
+      const response = await axios.get(`${this.REACT_APP_API_BASE_URL_DEV}/taux-cnaps/salarie`, {
+        auth: {
+          username: this.login,
+          password: this.pass,
+        },
+      })
+      return response
+    } catch (error) {
+      throw error
+    }
+  }
+}
