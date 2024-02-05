cf39cf3e6d207f01cb2a3d223d95dffa234e9d12
Mampionona33 <ramamps33@gmail.com>
2024-02-05 03:21:14 +0300

    wrap app with error boundary

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import App from './App'
+import ErrorBoundary from './components/ErrorHandling/ErrorBoundary'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
@@ -10,7 +11,9 @@ const root = createRoot(rootElement)
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </Provider>
   </StrictMode>,
 )
