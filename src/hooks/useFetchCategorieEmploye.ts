527cef9bf5a59f129d4f08689a996f3a4530e0c5
Mampionona33 <ramamps33@gmail.com>
2024-02-08 20:44:14 +0300

    fix useFechCategorieEmploye

--- src/hooks/useFetchCategorieEmploye.ts
+++ src/hooks/useFetchCategorieEmploye.ts
@@ -1,34 +1,20 @@
 import categorieEmployeService from '@src/services/CategorieEmployeService'
 import { useQuery } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
 
 const useFetchCategorieEmploye = () => {
-  const [data, setData] = useState(null)
-
-  const {
-    data: categoriesEmployes,
-    isLoading,
-    refetch,
-    isError,
-    error,
-  } = useQuery({
+  const { data, isLoading, refetch, isError, error } = useQuery({
     queryKey: ['categories_employes'],
     queryFn: async () => {
       try {
         const response = await categorieEmployeService.getAll()
-        setData(response.data)
         return response
       } catch (error) {
         throw error
       }
     },
   })
-  useEffect(() => {
-    if (data) {
-      setData(data.data)
-    }
-  }, [data])
-  return { data, isLoading, refetch, isError, error, categoriesEmployes }
+
+  return { data, isLoading, refetch, isError, error }
 }
 
 export default useFetchCategorieEmploye
