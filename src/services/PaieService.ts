2b70f6b2316f6bbfd23bebf7fc3c763777991039
Mampionona33 <ramamps33@gmail.com>
2024-02-09 04:10:16 +0000

    fix error PaieService

--- src/services/PaieService.ts
+++ src/services/PaieService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 class PaieService {
-  constructor(parameters) {}
+  constructor() {}
   REACT_APP_API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000'
   REACT_APP_API_BASE_URL_DEV = process.env.REACT_APP_API_BASE_URL_DEV || 'http://localhost:8000'
 
