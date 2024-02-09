9f8e495413911828c9ca755e4ad0b58742eb9152
Mampionona33 <ramamps33@gmail.com>
2024-02-09 04:25:17 +0300

    Fix error in useAppQuery

--- src/hooks/useAppQuery.ts
+++ src/hooks/useAppQuery.ts
@@ -1,37 +1,22 @@
-import { useEffect, useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 
 type CallbackFunction = () => Promise<any>
 
 const useAppQuery = (callback: CallbackFunction) => {
-  const [data, setData] = useState(null)
-  const [isLoading, setIsLoading] = useState(true)
-  const [errors, setErrors] = useState(null)
-
-  const { refetch, isError, error } = useQuery({
+  const { data, refetch, isError, error, isLoading } = useQuery({
     queryKey: ['genericQuery'],
     queryFn: async () => {
       try {
         const response = await callback()
-        setData(response.data)
-        setIsLoading(false)
-        return response
+
+        return response.data
       } catch (error) {
-        setErrors(error)
-        setIsLoading(false)
         throw error
       }
     },
   })
 
-  useEffect(() => {
-    if (isError) {
-      setErrors(error)
-      setIsLoading(false)
-    }
-  }, [isError, error])
-
-  return { data, isLoading, refetch, isError, errors }
+  return { data, isLoading, refetch, isError, error }
 }
 
 export default useAppQuery
