65b7dd9c105817251b2b6be7d833e1d78a329cc4
Mampionona33 <ramamps33@gmail.com>
2024-02-09 05:04:36 +0300

    fix error in useFetchModeDePaiement.tsx

--- src/hooks/useFetchModeDePayement.ts
+++ src/hooks/useFetchModeDePayement.ts
@@ -1,32 +1,20 @@
-import { useEffect, useState } from 'react'
 import modeDePayementService from '@src/services/ModeDePayementService'
 import { useQuery } from '@tanstack/react-query'
 
 const useFetchModeDePayement = (value: string) => {
-  const [modeDePayement, setModeDePayement] = useState(null)
-  const [errors, setErrors] = useState(null)
-
   const { data, isLoading, refetch, isError, error } = useQuery({
     queryKey: ['modeDePayement'],
     queryFn: async () => {
       try {
         const response = await modeDePayementService.getOneByVal(value)
-        setModeDePayement(response.data)
-        return response
+        return response.data
       } catch (error) {
-        setErrors(error)
         throw error
       }
     },
   })
 
-  useEffect(() => {
-    if (data) {
-      setModeDePayement(data.data)
-    }
-  }, [data])
-
-  return { modeDePayement, isLoading, refetch, isError, errors }
+  return { data, isLoading, refetch, isError, error }
 }
 
 export default useFetchModeDePayement
