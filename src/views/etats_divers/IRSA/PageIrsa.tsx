7511a1f3bd44bb63f9ddf6842f71bbb48f50a2d2
Mampionona33 <ramamps33@gmail.com>
2024-02-27 19:55:11 +0300

    créer carde IRSA

--- src/views/etats_divers/IRSA/PageIrsa.tsx
+++ src/views/etats_divers/IRSA/PageIrsa.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const PageIrsa = () => {
   return (
-    <div>
-      <p>Irsa</p>
+    <div className="flex">
+      <div className="flex flex-col shadow-sm bg-white rounded-sm">
+        <h3 className="bg-customRed-900 text-white text-lg px-4 py-2 capitalize">
+          Impôt sur les revenus salariaux et assimilés
+        </h3>
+        <div className="flex px-4 pb-4 pt-2">placeholders</div>
+      </div>
     </div>
   )
 }
