a26d1d873a87a78321518f73f475b559a85ce7eb
Mampionona33 <ramamps33@gmail.com>
2024-03-08 04:29:28 +0000

    créer reducer formPageOmsi

---
+++ src/redux/formPageOmsi/formPageOmsiReducer.ts
@@ -0,0 +1,22 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+import { IFormPageOmsi } from '@src/interfaces/interfaceFormPageOmsi'
+
+const initialState: IFormPageOmsi = {
+  periode: undefined,
+  annee: undefined,
+}
+
+const formPageOmsiSlice = createSlice({
+  name: 'pageOmsi',
+  initialState,
+  reducers: {
+    setFormPageOmsi: (state, action: PayloadAction<IFormPageOmsi>) => {
+      return { ...state, ...action.payload }
+    },
+    resetFormPageOmsi: (state) => {
+      return initialState
+    },
+  },
+})
+
+export const { setFormPageOmsi, resetFormPageOmsi } = formPageOmsiSlice.actions
