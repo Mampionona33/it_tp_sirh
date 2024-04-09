166672cf4a783b90ac3ea7fbad0a42fb7e3ec7e9
Mampionona33 <ramamps33@gmail.com>
2024-04-09 21:22:11 +0300

    wip: mise en place formulaire parametre paie

--- src/views/parametre/paies/ParametrePaie.tsx
+++ src/views/parametre/paies/ParametrePaie.tsx
@@ -1,29 +1,124 @@
 import React from 'react'
 import InputWithFloatingLabel from '@src/components/Inputs/InputFloatingLabel'
+import useFetchParametre from '@src/hooks/useFetchParametre'
+import useErrorFormatter from '@src/hooks/useErrorFormatter'
+import CustomCAlert from '@src/components/CustomAlert'
+import Loading from '@src/components/loadings/Loading'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import ButtonWithIcon from '@src/components/buttons/ButtonWithIcon'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+interface IParametrePaie {
+  plafondSme?: number
+  reductionChargeParEnfant?: number
+}
+
+const formParametrePaie: z.ZodType<IParametrePaie> = z.object({
+  plafondSme: z
+    .number()
+    .min(0)
+    .optional()
+    .refine((value) => value != null && value >= 0, {
+      message: 'Veuillez renseigner un plafond SME valide',
+    }),
+  reductionChargeParEnfant: z
+    .number()
+    .min(0)
+    .optional()
+    .refine((value) => value != null && value >= 0, {
+      message: 'Veuillez renseigner une valeur valide',
+    }),
+})
 
 const ParametrePaie = () => {
+  const {
+    data,
+    isError: isErrorFetchParameter,
+    isFetching: isFetchingFetchParameter,
+    error: errorFetchParameter,
+    isSuccess: isSuccessFetchParameter,
+  } = useFetchParametre()
+
+  const { handleSubmit, setValue, getValues, control } = useForm<IParametrePaie>({
+    resolver: zodResolver(formParametrePaie),
+  })
+
+  React.useEffect(() => {
+    if (isSuccessFetchParameter && data) {
+      // Mettre à jour les valeurs du formulaire avec les données de l'API
+      setValue('plafondSme', data?.plafond_sme ?? 0)
+      setValue('reductionChargeParEnfant', data?.reduction_charge_par_enfant ?? 0)
+    }
+  }, [data, isSuccessFetchParameter, setValue])
+
+  const formatError = useErrorFormatter()
+
+  if (isFetchingFetchParameter) {
+    return <Loading />
+  }
+
+  if (isErrorFetchParameter) {
+    return <CustomCAlert color="danger">{formatError(errorFetchParameter)}</CustomCAlert>
+  }
+
   return (
-    <div className="classeCard">
-      <h1 className="classeCardTitle">Parametres Paie</h1>
-      <form action="" className="classeCardBody">
-        <div className="flex gap-3">
-          <InputWithFloatingLabel
-            label="Plafond SME"
+    <form action="" className="flex flex-col gap-4">
+      <div className="classeCard">
+        <h1 className="classeCardTitle">Parametres Paie</h1>
+        <div className="classeCardBody grid gap-y-2 gap-x-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <Controller
+            control={control}
             name="plafondSme"
-            id="plafondSme"
-            placeholder="Plafond SME"
-            type="number"
+            render={({ field: { onChange, value }, fieldState: { error } }) => {
+              console.log(value)
+
+              return (
+                <div>
+                  <InputWithFloatingLabel
+                    label="Plafond SME"
+                    name="plafondSme"
+                    id="plafondSme"
+                    placeholder="Plafond SME"
+                    type="number"
+                    min={0}
+                    onChange={onChange}
+                    value={value?.toString() || '0'}
+                  />
+                  {error && <span className="text-sm text-customRed-800">{error.message}</span>}
+                </div>
+              )
+            }}
           />
-          <InputWithFloatingLabel
-            label="Reduction de charge par enfant"
+          <Controller
+            control={control}
             name="reductionChargeParEnfant"
-            id="reductionChargeParEnfant"
-            placeholder="Reduction de charge par enfant"
-            type="number"
+            render={({ field: { onChange, value }, fieldState: { error } }) => {
+              return (
+                <div>
+                  <InputWithFloatingLabel
+                    label="Reduction pour enfant"
+                    name="reductionChargeParEnfant"
+                    id="reductionChargeParEnfant"
+                    placeholder="Reduction pour enfant"
+                    type="number"
+                    min={0}
+                    onChange={onChange}
+                    value={value?.toString() || '0'}
+                  />
+                  {error && <span className="text-sm text-customRed-800">{error.message}</span>}
+                </div>
+              )
+            }}
           />
         </div>
-      </form>
-    </div>
+      </div>
+      <div className="classeCard">
+        <div className="classeCardBody">
+          <ButtonWithIcon type="submit" label="Enregistrer" />
+        </div>
+      </div>
+    </form>
   )
 }
 
