4847ccab18129de91c4c242c452ef7745f3de6b1
Mampionona33 <ramamps33@gmail.com>
2024-04-10 03:54:35 +0300

    wip: fini mise en place des différent cotisation

--- src/views/parametre/paies/ParametrePaie.tsx
+++ src/views/parametre/paies/ParametrePaie.tsx
@@ -12,6 +12,11 @@ import { zodResolver } from '@hookform/resolvers/zod'
 interface IParametrePaie {
   plafondSme?: number
   reductionChargeParEnfant?: number
+  cotisations?: {
+    name: string
+    part_salarie?: number
+    part_employeur?: number
+  }[]
 }
 
 const formParametrePaie: z.ZodType<IParametrePaie> = z.object({
@@ -29,6 +34,25 @@ const formParametrePaie: z.ZodType<IParametrePaie> = z.object({
     .refine((value) => value != null && value >= 0, {
       message: 'Veuillez renseigner une valeur valide',
     }),
+  cotisations: z
+    .array(
+      z.object({
+        name: z.string(),
+        part_salarie: z
+          .number()
+          .optional()
+          .refine((value) => value != null && value >= 0, {
+            message: 'Veuillez renseigner une valeur valide',
+          }),
+        part_employeur: z
+          .number()
+          .optional()
+          .refine((value) => value != null && value >= 0, {
+            message: 'Veuillez renseigner une valeur valide',
+          }),
+      }),
+    )
+    .optional(),
 })
 
 const ParametrePaie = () => {
@@ -50,6 +74,7 @@ const ParametrePaie = () => {
       // Mettre à jour les valeurs du formulaire avec les données de l'API
       setValue('plafondSme', data?.plafond_sme ?? 0)
       setValue('reductionChargeParEnfant', data?.reduction_charge_par_enfant ?? 0)
+      setValue('cotisations', data?.cotisations ?? [])
     }
   }, [data, isSuccessFetchParameter, setValue])
 
@@ -63,11 +88,15 @@ const ParametrePaie = () => {
     return <CustomCAlert color="danger">{formatError(errorFetchParameter)}</CustomCAlert>
   }
 
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.select()
+  }
+
   return (
     <form action="" className="flex flex-col gap-4">
       <div className="classeCard">
         <h1 className="classeCardTitle">Parametres Paie</h1>
-        <div className="classeCardBody grid gap-y-2 gap-x-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div className="classeCardBody grid gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           <Controller
             control={control}
             name="plafondSme"
@@ -85,6 +114,7 @@ const ParametrePaie = () => {
                     min={0}
                     onChange={onChange}
                     value={value?.toString() || '0'}
+                    onFocus={handleFocus}
                   />
                   {error && <span className="text-sm text-customRed-800">{error.message}</span>}
                 </div>
@@ -112,6 +142,46 @@ const ParametrePaie = () => {
               )
             }}
           />
+          {data?.cotisations?.map((cotisation, index) => (
+            <div key={index}>
+              <Controller
+                control={control}
+                name={`cotisations.${index}.part_salarie`}
+                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                  <InputWithFloatingLabel
+                    label={`Cotisation ${cotisation.name} salarie`}
+                    name={`cotisations[${index}].part_salarie`}
+                    id={`part_salarie_${index}`}
+                    placeholder={`Part salarié de ${cotisation.name}`}
+                    type="number"
+                    min={0}
+                    onChange={onChange}
+                    value={value?.toString() || '0'}
+                  />
+                )}
+              />
+            </div>
+          ))}
+          {data?.cotisations?.map((cotisation, index) => (
+            <div key={index}>
+              <Controller
+                control={control}
+                name={`cotisations.${index}.part_employeur`}
+                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                  <InputWithFloatingLabel
+                    label={`Cotisation ${cotisation.name} employeur`}
+                    name={`cotisations[${index}].part_employeur`}
+                    id={`part_employeur_${index}`}
+                    placeholder={`Part employeur de ${cotisation.name}`}
+                    type="number"
+                    min={0}
+                    onChange={onChange}
+                    value={value?.toString() || '0'}
+                  />
+                )}
+              />
+            </div>
+          ))}
         </div>
       </div>
       <div className="classeCard">
