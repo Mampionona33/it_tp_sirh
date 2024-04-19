f3a35b35438aebe8ea4ac2082d46ec964f97b711
Mampionona33 <ramamps33@gmail.com>
2024-04-19 12:11:45 +0000

    fix: Terminer remplacement CustomInputWithLabel par InputWithFloatingLabel dans CardPrimes

--- src/views/gestion-paie/valider-paie/ValiderCalculPaie/CardPrimes.tsx
+++ src/views/gestion-paie/valider-paie/ValiderCalculPaie/CardPrimes.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import CustomSection from '@src/components/CustomSection'
-import CustomInputWithLabel from '@src/components/Inputs/CustomInputWithLabel'
 import { useAppDispatch, useAppSelector } from '@src/hooks/useAppDispatch'
 import { setBulletinDePaie } from '@src/redux/bulletinDePaie/bulletinDePaieReducer'
 import { IBulletinDePaieProps } from '@src/interfaces/interfaceBulletinDePaie'
+import InputWithFloatingLabel from '@src/components/Inputs/InputFloatingLabel'
 
 const Body = () => {
   const dispatch = useAppDispatch()
@@ -12,7 +12,7 @@ const Body = () => {
   const handleInputChange = (name: string, value: string) => {
     const updatedPrimeEtGratification = {
       ...primeEtGratification,
-      [name]: parseInt(value) || 0,
+      [name]: parseFloat(value) || 0,
     }
     const totalPrimeEtGratification = Object.values(updatedPrimeEtGratification).reduce(
       (acc, currentValue) => acc + currentValue,
@@ -33,26 +33,26 @@ const Body = () => {
 
   return (
     <div className="w-full text-sm flex flex-col gap-4 p-4 ">
-      <CustomInputWithLabel
+      <InputWithFloatingLabel
         type="number"
         min={0}
-        required
         id="prime-assiduite"
         name="assiduite"
         label="Prime d’assiduité"
-        value={primeEtGratification!.assiduite || 0}
+        placeholder="Prime d’assiduité"
+        value={(primeEtGratification!.assiduite || '').toString()}
         onChange={(e) => handleInputChange(e.target.name, e.target.value)}
         onFocus={handleFocus}
       />
-      <CustomInputWithLabel
+      <InputWithFloatingLabel
         type="number"
         min={0}
-        required
         id="prime-excellence"
         name="excellence"
         label="Prime d'excellence"
+        placeholder="Prime d'excellence"
         onFocus={handleFocus}
-        value={primeEtGratification!.excellence || 0}
+        value={(primeEtGratification!.excellence || '').toString()}
         onChange={(e) => handleInputChange(e.target.name, e.target.value)}
       />
     </div>
