4aa9bb3eea8d07641269fdc09e5bbe09a5908b16
Mampionona33 <ramamps33@gmail.com>
2024-04-30 20:05:07 +0300

    fix error

--- src/interfaces/interfaceBtnDownloadOmsi.ts
+++ src/interfaces/interfaceBtnDownloadOmsi.ts
@@ -15,7 +15,7 @@ export interface DataOmsiProps {
   periode: string
   matricule: string
   nom: string
-  prenom: string
+  prenom?: string
   num_cnaps: string
   date_embauche: string
   date_debauche?: string
