88847d1c458e470bfd42669eb16d35cbe653d818
Mampionona33 <ramamps33@gmail.com>
2024-05-11 04:14:46 +0300

    ajout local db using mongosh

--- back-end/src/db.ts
+++ back-end/src/db.ts
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-dotenv.config({ path: './.env' })
+import path from 'path'
+
+const envFile = process.env.NODE_ENV === 'production' ? '.env' : '.env.local'
+console.log(path.resolve(__dirname, '..', envFile))
+
+dotenv.config({ path: path.resolve(__dirname, '..', envFile) })
 
 const MONGO_URI = process.env.MONGO_URI
+console.log('MONGO_URI', MONGO_URI)
 
 if (!MONGO_URI) {
   throw new Error('Please define the MONGO_URI environment variable inside .env.local')
